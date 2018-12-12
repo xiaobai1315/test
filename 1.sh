e7f5e71279396cd068e504c725344d0537201f34
xiaobai1315 <xiaobai1315@yeah.net>
2018-12-12 10:50:34 +0800

    edit

--- 1.sh
+++ 1.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
 
 echo 'hello world'
-echo 'hello world'
-echo 'hello world'
-echo 'hello world'
-echo 'hello world'
-
-
-print('fffffffff')
-print('fffffffff')
-print('fffffffff')
-
-nslog(@"branch log")
-nslog(@"branch log")
-nslog(@"branch log")
-nslog(@"branch log")
-nslog(@"branch log")
